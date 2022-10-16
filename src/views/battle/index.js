dc142085edcdac738f7f1a853fdd47407f3f445d
cy-moi <congyao119@gmail.com>
2022-10-16 15:15:55 +0200

    init obstacles

--- src/views/battle/index.js
+++ src/views/battle/index.js
@@ -1,5 +1,26 @@
 export const initBattle = () => {
-  const { app } = window;
+  const { app, addCraft, playground } = window;
   app.viewport.fit(window.it);
   app.viewport.follow(window.it);
+
+  const obstacles = [];
+
+  const seeds = Array.from({ length: 10 }, () =>
+    Math.floor(Math.random() * 500),
+  );
+
+  seeds.forEach((value, id) => {
+    obstacles.push(
+      addCraft({
+        id,
+        model: 'BasicShape',
+        host: playground,
+        type: 'rectangle',
+        position: { x: Math.random() * value, y: Math.random() * value },
+        size: { width: Math.random() * 100, height: Math.random() * 100 },
+        isStatic: true,
+        debug: false,
+      }),
+    );
+  });
 };
