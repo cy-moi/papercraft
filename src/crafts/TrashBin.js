adf54b70b94a8bd06e74da9c399046227cd4993c
cy-moi <congyao119@gmail.com>
2022-10-18 03:38:26 +0200

    back up

---
+++ src/crafts/TrashBin.js
@@ -0,0 +1,28 @@
+import EquipSlotHint from './EquipSlot';
+import { Sprite } from 'pixi.js';
+
+export default class TrashBin extends EquipSlotHint {
+  constructor(id) {
+    super();
+    this.id = id;
+    this.interactive = true;
+    this.buttonMode = true;
+  }
+
+  async init({ host, position, texture }) {
+    const { x, y } = position;
+    this.host = host;
+    this.x = x;
+    this.y = y;
+    this.hit = false;
+
+    this.sprite = Sprite.from(texture);
+
+    this.addChild(this.sprite);
+  }
+
+  update() {
+    if (this.hit) this.alpha = 1;
+    else this.alpha = 0.0;
+  }
+}
